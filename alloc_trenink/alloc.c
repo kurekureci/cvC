e34f172c73690afd120a61d29449f2375155300c
Michal Malaník <michal.malanik@closeit.cz>
2016-08-21 20:27:55 +0200

    nova fce na trenink dynamicke alokace pameti

---
+++ alloc_trenink/alloc.c
@@ -0,0 +1,5 @@
+/* zapnout program - > cekat na vstup z klavesnice
+ * ukladat kazdy radek - je zakonceny \n
+ * kdyz prijde radek .exit -> vsechny radky zadane vytisknout
+ * (naalokovat nove misto treba pro 10 znaku + kontrola, zda sem dosla na konec - pak realloc)
+ */
